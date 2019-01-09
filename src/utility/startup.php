592a3393bcd8f5c3dda03ebf5968722cb46f10df
ABCFS\DanielC <Daniel.Crouch@abcfinancial.com>
2019-01-09 14:28:27 -0600

    Made theme dynamic

--- src/utility/startup.php
+++ src/utility/startup.php
@@ -4,19 +4,12 @@ session_start();
 
 $project = "turingTest";
 $homeUrl = "http://turing.religionandstory.com";
+$style   = "red";
 
 function includeHeadInfo()
 {
-    include("$_SERVER[DOCUMENT_ROOT]/../common/html/head.html");
-    //if you ever use this theme in another site, move images to Common
-    echo '<link rel="shortcut icon" type="image/png" href="http://turing.religionandstory.com/images/logo.png"/>
-          <link rel="apple-touch-icon" sizes="57x57" href="http://turing.religionandstory.com/images/logo-57.png" />
-          <link rel="apple-touch-icon" sizes="60x60" href="http://turing.religionandstory.com/images/logo-60.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="http://turing.religionandstory.com/images/logo-72.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="http://turing.religionandstory.com/images/logo-114.png" />
-          <link rel="apple-touch-icon" sizes="120x120" href="http://turing.religionandstory.com/images/logo-120.png" />
-          <link rel="apple-touch-icon" sizes="144x144" href="http://turing.religionandstory.com/images/logo-144.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="http://turing.religionandstory.com/images/logo-152.png" />';
+    global $style;
+    include("$_SERVER[DOCUMENT_ROOT]/../common/html/head.php");
     echo '<link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">';
     echo '<style>
               :root {
