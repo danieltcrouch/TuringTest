dfbc38357a139f269c6f5b81dae9727e7d0149a3
ABCFS\DanielC <Daniel.Crouch@abcfinancial.com>
2019-01-09 13:43:55 -0600

    Changed theme to Red

--- src/utility/startup.php
+++ src/utility/startup.php
@@ -8,6 +8,24 @@ $homeUrl = "http://turing.religionandstory.com";
 function includeHeadInfo()
 {
     include("$_SERVER[DOCUMENT_ROOT]/../common/html/head.html");
+    echo '<link rel="shortcut icon" type="image/png" href="http://turing.religionandstory.com/images/logo.png"/>
+          <link rel="apple-touch-icon" sizes="57x57" href="http://turing.religionandstory.com/images/logo-57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="http://turing.religionandstory.com/images/logo-60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="http://turing.religionandstory.com/images/logo-72.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="http://turing.religionandstory.com/images/logo-114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="http://turing.religionandstory.com/images/logo-120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="http://turing.religionandstory.com/images/logo-144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="http://turing.religionandstory.com/images/logo-152.png" />';
+    echo '<link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">';
+    echo '<style>
+              :root {
+                 --mainColor:   #9D2235;
+                 --hoverColor:  #BD4255;
+                 --normalFont:  "Times New Roman", serif;
+                 --titleFont:   "Abril Fatface", serif;
+                 --titleWeight: normal;
+              }
+          </style>';
 }
 
 function includeHeader()
